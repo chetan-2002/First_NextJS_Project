b116f080d1b486503c3f8d938eec6237cf7d9d6d
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-12-23 08:59:36 +0530

    C:/Program Files/Git/ file updated

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,25 +1,3 @@
-// //domain.com/new-meetup
-// import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-// import { useRouter } from "next/router";
-// const NewMeetup = () => {
-//   const router = useRouter();
-//   const addMeetupHandler = async (enteredMeetupData) => {
-//     const response = await fetch('HTTP://localhost:3000/api/new-meetup', {
-//       method: "POST",
-//       body: JSON.stringify(enteredMeetupData),
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Access-Control-Allow-Origin": "*",
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data);
-//     router.push("/");
-//   };
-//   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
-// };
-// export default NewMeetup;
-
 // our-domain.com/new-meetup
 import { useRouter } from 'next/router';
 
